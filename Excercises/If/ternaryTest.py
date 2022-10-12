1b04dc56ba54280839cee77a67d181e551088674
tocee123 <toroktamas@azet.sk>
2022-10-12 16:39:39 +0100

    Terniray test added

---
+++ Excercises/If/ternaryTest.py
@@ -0,0 +1,28 @@
+# # # #[on_true] if [expression] else [on_false] 
+
+# fruit = "alapmeretezett"
+# if(10>20):
+#     fruit = "apple"
+
+
+# print(fruit)
+
+
+# fruit = "apple" if 10>20 else "banana"
+# print(fruit)
+
+a, b, c, d= 10, 20, "mango", "smolko"
+a = 10
+b = 20
+c = "mango"
+d = "torpike"
+
+min = 'a' if a<b else 'b'
+print(f"'a' if {a}<{b} else 'b' = {min}")
+print("'a' if ",a,"<",b," else 'b' = ",min, sep="")
+
+print(a,b,c,d, sep="...")
+
+# banana = "banana"
+# b = ord("A")
+# print(b)
