54485dc0d527af5f876190c03dad012b1a340689
tocee123 <93769005+tocee123@users.noreply.github.com>
2022-10-16 20:30:30 +0100

    Update blockOfCodes.py

--- Excercises/Variables/blockOfCodes.py
+++ Excercises/Variables/blockOfCodes.py
@@ -1,3 +1,32 @@
+# Oddelenie blokov
+Ked pisete blok kodu, tak to **vzdy** dajte len o jeden **tab** doprava (nie 1 medzera, nie dve medzery):
+
+namiesto
+```py
+maxNumber = 10
+if maxNumber == 20:
+      print("dvadsat")
+else:
+      print("nieco ine")
+```
+alebo
+```py
+maxNumber = 10
+if maxNumber == 20:
+ print("dvadsat")
+else:
+ print("nieco ine")
+```
+
+```py
+maxNumber = 10
+if maxNumber == 20:
+  print("dvadsat")
+else:
+  print("nieco ine")
+```
+
+
 x = 19
 
 if x >= 18 :
